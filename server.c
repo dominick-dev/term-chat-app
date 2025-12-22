1238c63ff2e2083b77d66ce26e9350382c8dacec
dominickdevincenzo <devindm101@gmail.com>
2025-12-22 00:39:31 -0500

    lied

--- server.c
+++ server.c
@@ -1,5 +1,6 @@
 #include <netinet/in.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 
 int main()
@@ -8,18 +9,21 @@ int main()
   int socketfd;
   struct sockaddr_in serv_addr;
 
-  // server steps
+  /* SERVER STEPS */
   // 1. create socket
   socketfd = socket(PF_INET, SOCK_STREAM, 0);
   if (socketfd == -1)
   {
     printf("Error creating socket\n");
   }
-  // 2. bind socket to ip and port
-  int server = bind(socketfd, )
-      // 3. listen for client connection
-      // 4. send/recieve data w/ client
-      // 5. close client socket when done
 
-      return 0;
+  // 2. bind socket to ip and port
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  int server = bind(socketfd, );
+
+  // 3. listen for client connection
+  // 4. send/recieve data w/ client
+  // 5. close client socket when done
+
+  return 0;
 }
