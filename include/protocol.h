bbcf59db3946c1cdf3095e939461ad4752fd896a
Dominick DeVincenzo <devindm101@gmail.com>
2026-01-25 22:06:23 -0500

    WIP: initial protocol

--- include/protocol.h
+++ include/protocol.h
@@ -10,15 +10,40 @@ typedef enum
     C2S_NEW_CLIENT
 } MessageType;
 
-// message header
+// generic message header
 typedef struct
 {
-    MessageType message_type;
+    uint8_t message_type; // cast enum to uint8_t
     uint16_t payload_length;
     uint16_t sequence_number;
     uint8_t version;
 } MessageHeader;
 
-//
+// different message type payloads
+typedef struct
+{
+    char username[20];
+} NewClientMsgPayload;
+
+// union on each message payload
+typedef union
+{
+    NewClientMsgPayload new_client;
+} MessagePayload;
+
+// generic message type
+typedef struct
+{
+    MessageHeader header;
+    MessagePayload payload;
+} Message;
+
+// serialize msgs
+void serialize_header(MessageHeader* header);
+void serialize_new_client_msg(Message* msg);
+
+// deserialize msgs
+void deserialize_header(MessageHeader* header);
+void deserialize_new_client_msg();
 
 #endif
---
+++ src/shared/protocol.c
@@ -0,0 +1,14 @@
+#include "../../include/protocol.h"
+
+/*
+ * Serialize msg
+ */
+void serialize_new_client_msg(Message* msg)
+{
+    // process
+}
+
+/*
+ * Deserialize msg
+ */
+void deserialize_new_client_msg();
